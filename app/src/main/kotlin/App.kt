cfaab410da07737a7d9a5b38237cff99c6c5ba0d
Hirankit Kanpatjarmit <hirankitkiki@gmail.com>
2025-06-23 12:29:57 +0700

    last

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,24 @@
+fun main(){
+    kop()
+}
+
+fun kop(){
+    var Kint: Int = 20_000
+    var Kboo: Boolean = true
+    var Kstr: String = "class24"
+    var Kdo: Double = 200.35
+    var KA: Any = "Class"
+    println("$Kint \n $Kboo \n $Kstr \n $Kdo \n $KA")
+    println("---------------------------------")
+
+    var a: Double = 7.0
+    var b: Double = 2.0
+    println(a%b)
+
+    var x: Int=10;
+    var y: Int=10;
+    x++
+    println(x) //11
+    ++y
+    println(y) //11
+}
